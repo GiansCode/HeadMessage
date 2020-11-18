dea9589a8f838cf69b6181a93a4fc896e7806ead
Frosty <frostyprime13@gmail.com>
2020-11-18 10:10:24 +0100

    Gradle Conversion,
    added a component list method so that it can be used on other platforms,
    
    Some general code cleanup

--- src/main/java/io/alerium/headmessage/HeadMessage.java
+++ src/main/java/io/alerium/headmessage/HeadMessage.java
@@ -19,6 +19,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.logging.Level;
 
 public final class HeadMessage {
 
@@ -26,95 +27,131 @@ public final class HeadMessage {
     private static BukkitAudiences audiences;
     private static File cacheFolder;
 
-    private HeadMessage() {
-    }
+    private HeadMessage() { }
 
     /**
      * This method inits the HeadMessage util
+     *
      * @param pl The Plugin that will use the HeadMessage util
      * @param cf The folder to save the cached skulls, can be null
      */
-    public static void init(Plugin pl, File cf) {
+    public static void init(final Plugin pl, final File cf) {
         plugin = pl;
         audiences = BukkitAudiences.create(pl);
         cacheFolder = cf;
-        
+
         if (cacheFolder != null && !cacheFolder.exists())
             cacheFolder.mkdirs();
     }
 
     /**
      * This method sends a message to a CommandSender with a skull of a Player
+     *
      * @param receiver The CommandSender that will receive the message
-     * @param skull The UUID of the Player with the skull
-     * @param height The height of the message
-     * @param center True if the message should be centered
-     * @param message A list of String(s) with the message
+     * @param skull    The UUID of the Player with the skull
+     * @param height   The height of the message
+     * @param center   True if the message should be centered
+     * @param message  A list of String(s) with the message
      */
-    public static void sendHead(CommandSender receiver, UUID skull, int height, boolean center, List<String> message) {
+    public static void sendHead(final CommandSender receiver, final UUID skull, final int height, final boolean center, final List<String> message) {
         if (plugin == null)
             return;
-        
+
         if (Bukkit.isPrimaryThread()) {
             Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> sendHead(receiver, skull, height, center, message));
             return;
         }
-        
-        List<Component> components = new ArrayList<>();
-        BufferedImage image = getCachedSkullImage(skull, height);
-        if (image == null)
-            return;
-        
+
+        final List<Component> components = getComponentList(skull, height, center, message);
+        if (components.isEmpty()) return;
+
+        components.forEach(component -> audiences.sender(receiver).sendMessage(Identity.nil(), component));
+    }
+
+    /**
+     * Returns the component list to be sent
+     *
+     * @param skull   The UUID of the Player with the skull
+     * @param height  The height of the message
+     * @param center  True if the message should be centered
+     * @param message A list of String(s) with the message
+     * @return A list of components to be sent to an audience
+     */
+    public static List<Component> getComponentList(final UUID skull, final int height, final boolean center, final List<String> message) {
+        final List<Component> components = new ArrayList<>();
+        final BufferedImage image = getCachedSkullImage(skull, height);
+        if (image == null) return components;
+
         for (int y = 0; y < height; y++) {
             Component line = Component.empty();
             for (int x = 0; x < height; x++) {
                 Color color = new Color(image.getRGB(x, y));
                 line = line.append(Component.text('\u2588', TextColor.color(color.getRed(), color.getGreen(), color.getBlue())));
             }
-            
+
             if (message != null && message.size() > y)
                 line = line.append(LegacyComponentSerializer.legacyAmpersand().deserialize(center ? centeredText(message.get(y), ChatPaginator.AVERAGE_CHAT_PAGE_WIDTH - height) : message.get(y)));
-        
+
             components.add(line);
         }
-        
-        components.forEach(component -> audiences.sender(receiver).sendMessage(Identity.nil(), component));
+
+        return components;
     }
 
-    private static BufferedImage getCachedSkullImage(UUID uuid, int size) {
+    /**
+     * @param uuid The UUID of the Player with the skull
+     * @param size The image size
+     * @return A BufferedImage retrieved from the cache or null if not present
+     */
+    private static BufferedImage getCachedSkullImage(final UUID uuid, final int size) {
         try {
             if (cacheFolder != null) {
-                File file = new File(cacheFolder, uuid + "_" + size + ".png");
+                final File file = new File(cacheFolder, uuid + "_" + size + ".png");
                 if (file.exists())
                     return ImageIO.read(file);
             }
-            
-            BufferedImage image = getSkullImage(uuid, size);
+
+            final BufferedImage image = getSkullImage(uuid, size);
             if (cacheFolder != null)
                 ImageIO.write(image, "PNG", new File(cacheFolder, uuid + "_" + size + ".png"));
-            
+
             return image;
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (final IOException ex) {
+            plugin.getLogger().log(Level.WARNING, "Failed to retrieve a BufferImage from Cache!", ex);
             return null;
         }
     }
-    
-    private static BufferedImage getSkullImage(UUID uuid, int size) throws IOException {
-        URL url = new URL(String.format("https://crafatar.com/avatars/%s?size=%s", uuid, size));
+
+    /**
+     * Returns the Skull image from Crafatar
+     *
+     * @param uuid The UUID of the Player with the skull
+     * @param size The image size
+     * @return BufferedImage of the Player's head avatar
+     * @throws IOException If the Image is corrupt
+     */
+    private static BufferedImage getSkullImage(final UUID uuid, final int size) throws IOException {
+        final URL url = new URL(String.format("https://crafatar.com/avatars/%s?size=%s", uuid, size));
         return ImageIO.read(url);
     }
-    
-    private static String centeredText(String s, int length) {
+
+    /**
+     * Returns a centered String with a specified chat length
+     *
+     * @param s      Message string
+     * @param length Chat length?
+     * @return Centered message string
+     */
+    private static String centeredText(final String s, final int length) {
         if (s.length() >= length)
             return s;
-        
-        int spaces = (length - s.length()) / 2;
-        StringBuilder sb = new StringBuilder();
+
+        final int spaces = (length - s.length()) / 2;
+        final StringBuilder sb = new StringBuilder();
         for (int i = 0; i < spaces; i++)
             sb.append(" ");
         sb.append(s);
         return sb.toString();
     }
-    
+
 }
